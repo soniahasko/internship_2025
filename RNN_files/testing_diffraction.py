a38b90fc2d7608af6b4c69eb8627545ddacb9100
Sonia Hasko <sh2968@princeton.edu>
2025-07-02 13:37:17 -0400

    New figures.

--- RNN_files/testing_diffraction.py
+++ RNN_files/testing_diffraction.py
@@ -1,32 +1,27 @@
-import sys
-import os
-
 # Add the parent directory to sys.path
 sys.path.append(os.path.abspath(".."))
 
-# Importing needed libraries
+# Importing necessary libraries
+import sys
+import os
 from matplotlib import pyplot as plt
 from RNN_files import Laitala_data_original_file
 import wfdb
 from wfdb.io import get_record_list
 from wfdb import rdsamp
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 from scipy.signal import resample_poly
+import xarray as xr
+import matplotlib.pyplot as plt
 from RNN_files import Laitala_data_original_file
 import tensorflow
 from tensorflow.keras import layers, models, Input
 import numpy as np
-import xarray as xr
-import matplotlib.pyplot as plt
 import sklearn
 from sklearn.model_selection import train_test_split
-from wfdb.processing import (
-    resample_singlechan,
-    find_local_peaks,
-    correct_peaks,
-    normalize_bound
-)
+from sklearn.metrics import f1_score
 
 diffraction_sim = False
 gauss_sim = True
@@ -35,13 +30,14 @@ visualize_example = False
 if diffraction_sim:
     path = '/nsls2/users/shasko/Repos/internship_2025/datasets/ds_combined_500_patterns_NaCl.nc'
     ds = xr.open_dataset(path, engine="netcdf4")
-    gaussians = ds["Intensities"]
-    binary = ds["binary_arr"]
+    gaussians = ds["Intensities"].values
+    binary = ds["binary_arr"].values
     x = ds["tth"].values
-    window_size = 11753
+
+    window_size = 11763
 
 if gauss_sim:
-    path = '/nsls2/users/shasko/Repos/internship_2025/datasets/math_functions_smalldataset.nc' 
+    path = '/nsls2/users/shasko/Repos/internship_2025/saved_data/gaussian_functions_smalldataset_varying_amps.nc' 
     ds = xr.open_dataset(path, engine="netcdf4")
 
     gaussians = ds["Gaussians"].values
@@ -49,7 +45,16 @@ if gauss_sim:
     print(type(binary))
 
     x = ds["x"].values
-    window_size = 990
+    window_size = 1000
+
+
+# Pad peaks with two 1s on either side
+for j in range(binary.shape[0]):
+    idx = np.where(binary[j] == 1)[0][0] # because there's only 1 "1" we can use [0][0]
+    binary[j][idx - 2] = 1
+    binary[j][idx - 1] = 1
+    binary[j][idx + 1] = 1
+    binary[j][idx + 2] = 1
 
 # Train-val, test split
 tv_gaussians, test_gaussians, tv_binary, test_binary = train_test_split(gaussians, binary, test_size=0.2, shuffle=False)
@@ -57,91 +62,25 @@ tv_gaussians, test_gaussians, tv_binary, test_binary = train_test_split(gaussian
 # Train, val split
 train_gaussians, val_gaussians, train_binary, val_binary = train_test_split(tv_gaussians, tv_binary, test_size=0.25, shuffle=False)
 
-# Make lists from train, val, test data
-gauss_signals_train = [signal for signal in train_gaussians]
-gauss_signals_val = [signal for signal in val_gaussians]
-gauss_signals_test = [signal for signal in test_gaussians]
+# Scale the data
+train_gaussians_sc = np.zeros_like(train_gaussians)
+val_gaussians_sc = np.zeros_like(val_gaussians)
+test_gaussians_sc = np.zeros_like(test_gaussians)
 
-binary_labels_train = [label for label in train_binary]
-binary_labels_val = [label for label in val_binary]
-binary_labels_test = [label for label in test_binary]
+for j in range(train_gaussians.shape[0]):
+    max_inten = np.max(train_gaussians[j])
+    min_inten = np.min(train_gaussians[j])
+    train_gaussians_sc[j] = (train_gaussians[j] - min_inten) / (max_inten - min_inten)
 
-# Data generator adapted from Laitala et al.
-def data_generator(signals, labels, win_size, batch_size):
-   
-    while True:
-        X, y = [], []
+for j in range(val_gaussians.shape[0]):
+    max_inten = np.max(val_gaussians[j])
+    min_inten = np.min(val_gaussians[j])
+    val_gaussians_sc[j] = (val_gaussians[j] - min_inten) / (max_inten - min_inten)
 
-        while len(X) < batch_size:
-            i = np.random.randint(0, len(signals))
-            sig = signals[i]
-            lbl = labels[i]
-
-            if len(sig) <= win_size + 4:
-                continue  # skip short signals
-
-            start = np.random.randint(2, len(sig) - win_size - 2)
-            end = start + win_size
-
-            data_win = sig[start:end]
-            label_win = lbl[start:end]
-
-            # Pad 1s ±2 samples around every 1 in label_win
-            padded_label = label_win.copy()
-            ones = np.where(label_win == 1)[0]
-            for p in ones:
-                for offset in [-2, -1, 1, 2]:
-                    if 0 <= p + offset < win_size:
-                        padded_label[p + offset] = 1
-
-            # Normalize signal window to (0, 1)
-            # data_win = normalize_bound(data_win, lb=0, ub=1)
-
-            # Normalized locally
-            data_win = (data_win - np.min(data_win))/(np.max(data_win) - np.min(data_win))
-
-            # Normalized globally
-            # low = np.min(signals)
-            # high = np.max(signals)
-            # data_win = (data_win - low)/(high - low)
-
-            X.append(data_win)
-            y.append(padded_label)
-
-        X = np.array(X).reshape(batch_size, win_size, 1)
-        y = np.array(y).reshape(batch_size, win_size, 1).astype(int)
-
-        yield X, y
-
-if visualize_example:
-    gen = data_generator(gauss_signals_train, binary_labels_train, win_size=window_size, batch_size=64)
-    X_batch, y_batch = next(gen)
-    n = next(gen)
-
-    # Plot 4 training examples with labels
-    fig, axs = plt.subplots(2, 2)
-    fig.set_figheight(10), fig.set_figwidth(18)
-    fig.suptitle('Some examples of training data with labels', size=20)
-
-    # first index refers to whether it's an input or label, so noisy signal is input and peaks are labels
-    axs[0, 0].plot(n[0][0], color='orange')
-    axs[0, 0].plot(n[1][0]+1, color='green')
-
-    axs[0, 1].plot(n[0][1], color='purple')
-    axs[0, 1].plot(n[1][1]+1, color='blue')
-
-    axs[1, 0].plot(n[0][2], color='yellow')
-    axs[1, 0].plot(n[1][2]+1, color='red')
-
-    axs[1, 1].plot(n[0][3], color='black')
-    axs[1, 1].plot(n[1][3]+1, color='grey')
-
-    # Hide x labels and tick labels for top plots and y ticks for right plots.
-    for ax in axs.flat:
-        ax.label_outer()
-
-    plt.show()
-
+for j in range(test_gaussians.shape[0]):
+    max_inten = np.max(test_gaussians[j])
+    min_inten = np.min(test_gaussians[j])
+    test_gaussians_sc[j] = (test_gaussians[j] - min_inten) / (max_inten - min_inten)
 
 n_batch, n_timesteps, n_input_dim = 64, window_size, 1
 
@@ -151,6 +90,7 @@ model.add(layers.Bidirectional(layers.LSTM(32, return_sequences=True)))
 model.add(layers.Bidirectional(layers.LSTM(32, return_sequences=True)))
 model.add(layers.Dense(1, activation='sigmoid'))
 
+# Compile model
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['acc'])
@@ -167,32 +107,101 @@ es_callback = tf.keras.callbacks.EarlyStopping(monitor='loss',
                                               patience=3,
                                               verbose=2)
 
-# Generate training, validation, and testing data
-training_gen = data_generator(gauss_signals_train, binary_labels_train, win_size=n_timesteps, batch_size=n_batch)
-validation_gen = data_generator(gauss_signals_val, binary_labels_val, win_size=window_size, batch_size=n_batch)
-testing_gen = data_generator(gauss_signals_test, binary_labels_test, win_size=n_timesteps, batch_size=n_batch)
+# Reshape intensity data
+train_gaussians_reshaped = train_gaussians_sc.reshape(train_gaussians_sc.shape[0], train_gaussians_sc.shape[1], 1)
+val_gaussians_reshaped = val_gaussians_sc.reshape(val_gaussians_sc.shape[0], val_gaussians_sc.shape[1], 1)
+test_gaussians_reshaped = test_gaussians_sc.reshape(test_gaussians_sc.shape[0], test_gaussians_sc.shape[1], 1)
+
+# Reshape labels
+train_binary_reshaped = train_binary.reshape(train_binary.shape[0], train_binary.shape[1], 1)
+val_binary_reshaped = val_binary.reshape(val_binary.shape[0], val_binary.shape[1], 1)
+test_binary_reshaped = test_binary.reshape(test_binary.shape[0], test_binary.shape[1], 1)
 
 # Train model
-model.fit(training_gen,
-          steps_per_epoch=40,
-          epochs=20, 
-          validation_data=validation_gen,
-          validation_steps=10,
+model.fit(x=train_gaussians_reshaped,
+          y=train_binary_reshaped,
+          batch_size=64,
+          epochs=40, 
+          validation_data=(val_gaussians_reshaped, val_binary_reshaped),
           callbacks=[cp_callback, es_callback])
 
-gauss_test, label_test = next(testing_gen)
 
-binary_pred = model.predict(gauss_test) # Make predictions
+# Make predictions on held-out test set
+binary_pred = model.predict(test_gaussians_reshaped,
+                            verbose=2)
 
-plt.figure()
+def f1_w_threshold(threshold, test, pred):
+    pred_adjusted = (pred >= threshold).astype(int)
+    test = test.astype(int)
+    f1 = f1_score(test.squeeze(), pred_adjusted.squeeze(), average='micro')
+    return f1
 
-for j in range(1):
-    plt.plot(gauss_test[j], color='green', label='Signal')
-    plt.plot(binary_pred[j] + 1, color='orange', label='Prediction Probabilities')
-    plt.plot(label_test[j] + 2, color='purple', label='True Labels')
+f1_gauss = f1_w_threshold(threshold=0.3, test=test_binary_reshaped, pred=binary_pred)
+print(f'f1 for gaussian test set is {f1_gauss}')
 
-plt.tick_params(top=True, bottom=True, left=True, right=True, direction='in')
-plt.legend(loc='upper left', bbox_to_anchor=(1.05, 1))
-plt.tight_layout()
-# plt.savefig('test_results_multigauss1.png')
-plt.show()
+# Evaulate on non-Gauss data. In this case, Lorentzian signals.
+path = '/nsls2/users/shasko/Repos/internship_2025/saved_data/lorentzian_functions_smalldataset.nc' 
+ds = xr.open_dataset(path, engine="netcdf4")
+
+intensities_lor = ds["Intensities"].values
+binary = ds["BinaryArr"].values
+x = ds["x"].values
+window_size = 1000
+
+# Scale signals
+intensities_lor_sc = np.zeros_like(intensities_lor)
+
+for j in range(intensities_lor.shape[0]):
+    max_inten = np.max(intensities_lor[j])
+    min_inten = np.min(intensities_lor[j])
+    intensities_lor_sc[j] = (intensities_lor[j] - min_inten) / (max_inten - min_inten)
+
+# Reshape intensities
+intensities_lor_sc_reshaped = intensities_lor_sc.reshape(intensities_lor_sc.shape[0],
+                                                         intensities_lor_sc.shape[1],
+                                                         1)
+
+# Reshape labels
+binary_lor_reshaped = binary.reshape(binary.shape[0],
+                                 binary.shape[1],
+                                 1)
+
+# Make prediction
+pred_lor = model.predict(intensities_lor_sc_reshaped,
+                         verbose=2)
+
+f1_lor = f1_w_threshold(threshold=0.3, test=binary_lor_reshaped, pred=pred_lor)
+print(f'f1 for lorentzian test set is {f1_lor}')
+
+
+# Evaulate on non-Gauss data. In this case, cubic NaCl diffraction signals.
+path = '/nsls2/users/shasko/Repos/internship_2025/datasets/ds_combined_500_patterns_NaCl.nc'
+ds = xr.open_dataset(path, engine="netcdf4")
+intensities_diff = ds["Intensities"].values
+binary_diff = ds["binary_arr"].values
+x = ds["tth"].values
+
+window_size = 11763
+
+# Scale signals
+intensities_diff_sc = np.zeros_like(intensities_diff)
+
+for j in range(intensities_diff.shape[0]):
+    max_inten = np.max(intensities_diff[j])
+    min_inten = np.min(intensities_diff[j])
+    intensities_diff_sc[j] = (intensities_diff[j] - min_inten) / (max_inten - min_inten)
+
+# Reshape signals and labels
+intensities_diff_sc_reshaped = intensities_diff_sc.reshape(intensities_diff_sc.shape[0],
+                                                           intensities_diff_sc.shape[1],
+                                                           1)
+binary_diff_reshaped = binary_diff.reshape(binary_diff.shape[0],
+                                           binary_diff.shape[1],
+                                           1)
+
+# Make predictions
+pred_diff = model.predict(intensities_diff_sc_reshaped,
+                         verbose=2)
+
+f1_diff = f1_w_threshold(threshold=0.3, test=binary_diff_reshaped, pred=pred_diff)
+print(f'f1 for lorentzian test set is {f1_diff}')
